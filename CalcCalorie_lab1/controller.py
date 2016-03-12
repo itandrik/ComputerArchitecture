a7a496670ac5fea316b61a1638a703b7343d111e
lowbite <newbite@ukr.net>
2016-03-12 22:23:25 +0200

    final version

--- CalcCalorie_lab1/controller.py
+++ CalcCalorie_lab1/controller.py
@@ -1,26 +1,29 @@
+# Controlling work of program
+
 import model
 import view
 
 
+# Transfers all choices to model
+# and model result to view, where result printing
 def main():
-	while(True):
-		view.createMenu()
-		try:
-			point = input()
-			if(point == 1):
-				gender = view.readGender()
-				weight = view.readWeight()
-				height = view.readHeight()
-				age = view.readAge()
-				pa = view.readPA()
-				view.getInfo(model.calculate_calories(gender, weight, height, age, pa))
-				raw_input()
-			elif(point == 2):
-				break
-			else:
-				print("Wrong input! Try again")
-		except NameError:
-			print ("Wrong input! Try again")
+    while(True):
+        view.createMenu()
+        try:
+            point = input()
+            if(point == 1):
+                gender = view.readGender()
+                weight = view.readWeight()
+                height = view.readHeight()
+                age = view.readAge()
+                pa = view.readPA()
+                view.getInfo(model.calculate_calories(gender, weight, height, age, pa))
+                raw_input()
+            elif(point == 2):
+                break
+            else:
+                print("Wrong input! Try again")
+        except NameError:
+            print ("Wrong input! Try again")
 
-main()		
-		
+main()
--- CalcCalorie_lab1/view.py
+++ CalcCalorie_lab1/view.py
@@ -1,29 +1,36 @@
+# Printing all points to
+# choose to calculate calories
+
+
 str1 = ["Gender:", "1.Male", "2.Female"]
 str2 = ["Physical activity:", "1.Minimum physical activity",
         "2. 3 times per week", "3. 5 times per week",
         "4. Every day", "5. 2 times per day"]
 
 
+# Printing menu of age choice
 def readAge():
     while(True):
         try:
             age = input("Age: ")
-            if(age >= 13 & age < 80):
+            if((age >= 13) & (age < 80)):
                 break
             else:
-                print("Unreal age! Repeat please, age should be between 13 and 80")
+                print("Unreal age!")
+                print("Repeat please, age should be between 13 and 80")
         except NameError:
             print("Wrong input! Repeat please")
     return age
 
 
+# Printing menu of gender choice
 def readGender():
     while(True):
         for i in str1:
             print i
         try:
             gender = input()
-            if(gender > 0 & gender < 3):
+            if((gender > 0) & (gender < 3)):
                 break
             else:
                 print("Wrong input! Repeat please")
@@ -32,11 +39,12 @@ def readGender():
     return gender
 
 
+# Printing menu of weight choice
 def readWeight():
     while(True):
         try:
             weight = input("Weight: ")
-            if(weight >= 35 & weight < 250):
+            if((weight >= 35) & (weight < 250)):
                 break
             else:
                 print("Repeat please, weight should be between 35 and 250")
@@ -45,11 +53,12 @@ def readWeight():
     return weight
 
 
+# Printing menu of height choice
 def readHeight():
     while(True):
         try:
             height = input("Height in cm: ")
-            if(height >= 120 & height < 250):
+            if((height >= 120) & (height < 250)):
                 break
             else:
                 print("Repeat please, height should be between 120 and 250")
@@ -58,13 +67,14 @@ def readHeight():
     return height
 
 
+# Printing menu of physical activity choice
 def readPA():
     while(True):
         for i in str2:
             print i
         try:
             pa = input()
-            if(pa >= 1 & pa <= 5):
+            if((pa >= 1) & (pa <= 5)):
                 break
             else:
                 print("Wrong input! Repeat please...")
@@ -73,11 +83,12 @@ def readPA():
     return pa
 
 
+# Printing main menu
 def createMenu():
     print("1. Calculate your calories")
     print("2. Exit")
 
 
+# Printing result of program
 def getInfo(result):
-    res = result + " kilocalories"
-    print (res)
+    print (str(result) + " kilocalories")
