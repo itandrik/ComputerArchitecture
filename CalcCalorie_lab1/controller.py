a4925aaae9ab5efadc0787e97a0ca8e4cc3312d6
Bodya95 <x.bodya@bk.ru>
2016-03-12 19:29:20 +0200

    Creating contorller

--- CalcCalorie_lab1/controller.py
+++ CalcCalorie_lab1/controller.py
@@ -1,1 +1,18 @@
-print "test"
+import model
+import view
+
+
+def main(view): 
+  while(True): 
+	createMenu()
+	point = input()
+	if(point == 1):
+		calculate_calories(readGender, readWeigh, readHeight, readAge, readPA)
+	else(point == 2):
+		break
+	else
+		print("Wrong input! Try again")
+
+
+main()		
+		
