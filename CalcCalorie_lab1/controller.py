6db034bc697034a44d3c353f13702d41365403f6
Bodya95 <x.bodya@bk.ru>
2016-04-01 01:16:56 +0300

    Making pep8 and adding the commentaries

--- CalcCalorie_lab1/controller.py
+++ CalcCalorie_lab1/controller.py
@@ -4,28 +4,31 @@ import model
 import view
 import serialize
 
+
 # Transfers all choices to model
-# and model result to view, where result printing
+# and model result to view, where result printing.
+# Creating possibility to change cpnfig files and to dump some information
 def main():
     while True:
         point = view.createMenu()
         if point == 1:
-            gender = view.readGender()
-            weight = view.readWeight()
-            height = view.readHeight()
-            age = view.readAge()
-            pa = view.readPA()
+            gender = view.read_gender()
+            weight = view.read_weight()
+            height = view.read_height()
+            age = view.read_age()
+            pa = view.read_pa()
             calories = model.calculate_calories(
                 gender, weight, height, age, pa)
-            view.getInfo(calories)
+            view.get_info(calories)
             data = (gender, weight, height, age, pa, calories)
             raw_input()
-            if view.isDump(): serialize.dump(data)
+            if view.is_dump():
+                serialize.dump(data)
         elif point == 2:
             serialize.load()
         elif point == 3:
-            data = view.changeSerializationFile()
-            serialize.change_config(data[0],data[1])
+            data = view.read_serialization_file()
+            serialize.change_config(data[0], data[1])
         elif point == 4:
             break
 
